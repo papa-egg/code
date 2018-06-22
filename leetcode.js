f916c08c7682ed08673ac2344d1dcd2e820b1fee
jinyang <1027493999@qq.com>
2018-06-22 16:22:41 +0800

    maximalRectangle

--- leetcode.js
+++ leetcode.js
@@ -62,7 +62,6 @@ function threeSum(nums) {
 
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-
             nums.forEach((item, index) => {
                 if (index == i || index == j) return;
 
